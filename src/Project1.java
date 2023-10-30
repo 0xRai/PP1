1892c10a9b7508ddede019ead7e6a186b7cfaa61
Rai <robertblake.hardy@outlook.com>
2023-10-30 15:01:04 +0900

    Updated exception catching in Project1.java

--- src/Project1.java
+++ src/Project1.java
@@ -2,10 +2,12 @@
 Name: Robert Hardy
 Project Name: Programming Project 1 - CMIS 215
 Date: 28 Oct 2023
-Class Description: Primary class that handles user input
+Class Description: Primary class that handles user input and converts it to a Player object and then finds the tallest
+player that is LTE to the average age of the list of Players.
 */
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Project1 {
@@ -18,17 +20,37 @@ public class Project1 {
         // Loops through num of players and creates Player obj for each and gets sum of age from all players
         int sumOfAge = 0;
         int numOfPlayers = input.nextInt();
-        for (int i = 0; i < numOfPlayers; i++){
-            System.out.printf("What is player %d's name (first and last):\n", i+1);
+        for (int i = 0; i < numOfPlayers; i++) {
+            System.out.printf("What is player %d's name (first and last):\n", i + 1);
             String name = input.next();
-            name += " "+input.next();
-            System.out.printf("What is %s's age: ", name);
-            int age = input.nextInt();
-            System.out.printf("What is %s's height (feet inches) ", name);
-            int feet = input.nextInt();
-            int inches = input.nextInt();
+            name += " " + input.next();
+            int age;
+            int feet;
+            int inches;
+            while (true) {
+                try {
+                    System.out.printf("What is %s's age: ", name);
+                    age = input.nextInt();
+                    break;
+                } catch (InputMismatchException ex) {
+                    input.next();
+                    System.out.println("Please enter a valid integer\n");
+                }
+            }
+            while (true) {
+                try {
+                    System.out.printf("What is %s's height (feet inches) ", name);
+                    feet = input.nextInt();
+                    inches = input.nextInt();
+                    break;
+                } catch (InputMismatchException ex) {
+                    input.next();
+                    System.out.println("Please enter a valid integer\n");
+                }
+            }
+
             Player player = new Player(name,
-                    new Height(feet,inches),
+                    new Height(feet, inches),
                     age);
             sumOfAge += player.getAge();
             arrayList.add(player);
@@ -50,7 +72,7 @@ public class Project1 {
         }
 
         // Prints tallest player's information: name, age, height (feet'inches")
-        if (currentTallestPlayer != null){
+        if (currentTallestPlayer != null) {
             System.out.printf("\nAverage age of players:\t%f\n", avgAge);
             System.out.println("Tallest player less or equal to average age:");
             System.out.println(currentTallestPlayer);
